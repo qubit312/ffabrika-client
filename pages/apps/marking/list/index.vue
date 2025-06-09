07cfe8154a0b4de76b1e3f77f1c7ece1342ec5fb
GanievIbraim <ganiev.i.i.21@gmail.com>
2025-06-09 10:18:17 +0300

    ttt

--- pages/apps/marking/list/index.vue
+++ pages/apps/marking/list/index.vue
@@ -72,7 +72,7 @@ const deleteProduct = async (id: number) => {
   <div>
     <!-- marking -->
     <VCard
-      title="Этикетки Проверка автодеплоя"
+      title="Этикетки Проверка автодеплоя 2"
       class="mb-6"
     >
       <VDivider />
