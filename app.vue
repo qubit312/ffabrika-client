f44475e798816f3655ed65d913c3671a5de7251a
GanievIbraim <ganiev.i.i.21@gmail.com>
2025-06-09 14:21:12 +0300

    2

--- app.vue
+++ app.vue
@@ -18,7 +18,7 @@ if (isMobile)
 </script>
 
 <template>
-  <!-- Тестируем pull-->
+  <!-- Тестируем pull 2-->
   <VLocaleProvider :rtl="configStore.isAppRTL">
     <!-- ℹ️ This is required to set the background color of active nav link based on currently active global theme's primary -->
     <VApp :style="`--v-global-theme-primary: ${hexToRgb(global.current.value.colors.primary)}`">
